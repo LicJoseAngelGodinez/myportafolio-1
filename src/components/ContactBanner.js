364fd9cf50f5605c3194f9325b536e231c377d91
AngelGodinez-lenovo <licangelgodinez@hotmail.com>
2021-06-11 17:59:04 -0500

    [CONTACT BANNER]

---
+++ src/components/ContactBanner.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import * as Constants from '../constants';
+import PText from './PText';
+import Button from './Buttons';
+
+const ContactBannerStyles = Constants.ContactBannerStyles;
+
+export default function ContactBanner() {
+    return (
+        <ContactBannerStyles>
+            <div className="container">
+                <div className="contactBanner__wrapper">
+                    <PText>
+                        Tienes un proyecto en mente
+                    </PText>
+                    <h3 className="contactBanner__heading">
+                        Podemos hacerlo realidad
+                    </h3>
+                    <Button
+                        btnText="Contactar ahora"
+                        btnLink="/contact"
+                        outline={false}
+                    />
+                </div>
+            </div>
+        </ContactBannerStyles>
+    )
+}
--- src/constants.js
+++ src/constants.js
@@ -564,3 +564,25 @@ export const TestimonialSectionStyle = styled.div`
         
     }
 `;
+
+/*
+ * Contact Banner
+ */
+export const ContactBannerStyles = styled.div`
+    padding: 10rem 0;
+    .contactBanner__wrapper{
+        background-color: var(--deep-dark);
+        border-radius: 12px;
+        padding: 5rem 0rem;
+        text-align: center;
+    }
+    .contactBanner__heading{
+        font-size: 4rem;
+        margin-bottom: 2rem;
+    }
+    @media only screen and (max-width: 768px){
+        .contactBanner__heading{
+            font-size: 2.8rem;
+        }
+    }
+`;
